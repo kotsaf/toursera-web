63054e7293b22878e902351b05640be2f9745cc1
safqw <safqwer.136@gmail.com>
2025-04-09 02:57:36 +0300

    Доработка и изменения

--- webshop/main/urls.py
+++ webshop/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'), 
+    path('tours', views.tours, name='tours'),
+    path('feedbacks', views.feedbacks, name='feedbacks'),
+    path('contacts', views.contacts, name='contacts')
 ]
--- webshop/main/views.py
+++ webshop/main/views.py
@@ -5,3 +5,12 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def tours(request):
+    return render(request, 'tours.html')
+
+def feedbacks(request):
+    return render(request, 'feedbacks.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
