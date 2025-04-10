d9cb787a6351fc259485d9178ee2639ed4ea88f3
safqw <safqwer.136@gmail.com>
2025-04-11 00:45:25 +0300

    Added new features: almist finished the main page, rebuild structure

--- webshop/main/static/main/js/scripts.js
+++ webshop/main/static/main/js/scripts.js
@@ -1,18 +1,8 @@
-const sec2 = document.getElementById('jsjs')
 const sec1 = document.querySelector('.name-bio')
 const header = document.querySelector('header')
 const header2 = document.querySelector('#header2')
 
 
-
-function handleClick() {
-  anime({
-    targets: sec2,
-    translateX: '300px',
-    loop: true,
-    duration: 3000,
-})
-};
 
 setTimeout(function bioname() {
     anime({
