cb519931b15f42077a65325e62588fefde4fffe1
Safar <safqwer.136@gmail.com>
2025-04-18 23:19:09 +0300

    Update settings.py

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -1,14 +1,10 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cb8%cgh%nkp8b5^@@b4g%8@fd=&3mrufwu^w%+&-yy)7%mi46m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
